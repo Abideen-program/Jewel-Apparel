7f010b51ef34d0df5ed6ad03d2375987788c2f3c
Abideen Olafimihan <olafimihana@gmail.com>
2022-06-27 20:17:34 +0100

    updating App.js

--- src/App.js
+++ src/App.js
@@ -3,43 +3,44 @@ const App = () => {
 
   const categories = [
     {
-      id: 1,
-      title: 'Hats'
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
     },
-
     {
-      id: 2,
-      title: 'Jackets'
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
     },
-
     {
-      id: 3,
-      title: 'Sneakers'
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
     },
-
     {
-      id: 4,
-      title: 'Men'
+      "id": 4,
+      "title": "women",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
     },
-
     {
-      id: 5,
-      title: 'Women'
-    },
-
-    // {
-    //   id: 6,
-    //   title: 'Kids'
-    // },
+      "id": 5,
+      "title": "men",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
   ]
   
   return(
     <div className='categories-container'>
         {
-            categories.map(({ id, title}) => {
+            categories.map(({ id, title, imageUrl}) => {
                 return(
                     <div className='category-container' key={id}>
-                        <div className='background-image'/>
+                        <div className='background-image' 
+                           style={{
+                            
+                           }}
+                        />
+
                         <div className='category-body-container'>
                             <h2>{title}</h2>
                             <p>Shop Now</p>
