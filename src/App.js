40aa3a49083fcabfc1379897ef011577ec27341d
Abideen Olafimihan <olafimihana@gmail.com>
2022-06-30 09:18:00 +0100

    updating files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import Navigator from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
-  
 
 const Shop = () => {
-    return(
-        <h1>This is the Shop Page</h1>
-    )
-}
+  return <h1>This is the Shop Page</h1>;
+};
 
 const App = () => {
   return (
