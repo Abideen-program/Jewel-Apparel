a3f8e174ff6e5bd0247228cbcb273c8b1bc2b197
Abideen Olafimihan <olafimihana@gmail.com>
2022-07-01 08:40:12 +0100

    adding the sign-up page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Navigator from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 const Shop = () => {
   return <h1>This is the Shop Page</h1>;
@@ -12,6 +13,7 @@ const App = () => {
       <Route path="/" element={<Navigator />}>
         <Route index element={<Home />} />
         <Route path="/Shop" element={<Shop />} />
+        <Route path="/sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,8 @@
+
+const SignIn = () => {
+    return (
+        <h1>This is the sign up page</h1>
+    )
+}
+
+export default SignIn
