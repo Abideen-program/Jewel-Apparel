11158e30b985aaed4cb3da66f7e3477cadd4a57d
Abideen Olafimihan <olafimihana@gmail.com>
2022-07-04 17:53:05 +0100

    creating the firebase file

---
+++ src/utilities/firebase/firebase.utilities.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBjqPk8Dk2EMDfYiBVov6AQjd-swwaADuU",
+  authDomain: "jewel-clothing-line.firebaseapp.com",
+  projectId: "jewel-clothing-line",
+  storageBucket: "jewel-clothing-line.appspot.com",
+  messagingSenderId: "438105889005",
+  appId: "1:438105889005:web:db15732b7c33604fc254e4",
+};
+
+const app = initializeApp(firebaseConfig);
