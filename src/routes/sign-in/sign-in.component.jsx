dc5c80ea2b7651b56f4cdac27d4aeb3ec99d6407
Abideen Olafimihan <olafimihana@gmail.com>
2022-07-05 18:25:20 +0100

    leveraging the sign-in function

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,8 +1,14 @@
+import { signInWithGooglePopup } from '../../utilities/firebase/firebase.utilities'
 
 const SignIn = () => {
+    const logGoogleUser = async () => {
+        const response = await signInWithGooglePopup()
+        console.log(response)
+    }
     return (
         <div>
             <h1>This is the sign up page</h1>
+            <button onClick={logGoogleUser}>Sign up with Google</button>
         </div>
     )
 }
