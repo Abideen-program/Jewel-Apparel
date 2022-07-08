06ae51bd91b324108bf68ee59a80988b341380ee
Abideen Olafimihan <olafimihana@gmail.com>
2022-07-08 11:41:02 +0100

    updating sign-up page

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,26 +1,66 @@
-import {useState} from 'react'
+import { useState } from "react";
+
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
 const SignUp = () => {
+  const [formFields, setformFields] = useState(defaultFormFields);
 
-    return(
-        <div>
-            <h2>Sign up with your eamil and password</h2>
-            <form onSubmit={()=> {}}>
-                <label>Display Name</label>
-                <input type="text" required />
+  const { displayName, email, password, confirmPassword } = formFields;
+    console.log(formFields)
+  const changeHandler = (event) => {
+    const { name, value } = event.target;
+    setformFields({ ...formFields, [name]: value });
+  };
 
-                <label>Email</label>
-                <input type="text" required />
+  return (
+    <div>
+      <h2>Sign up with your eamil and password</h2>
+      <form onSubmit={() => {}}>
+        <label>Display Name</label>
+        <input
+          type="text"
+          required
+          onChange={changeHandler}
+          name="displayName"
+          value={displayName}
+        />
 
-                <label>Password</label>
-                <input type="text" required />
+        <label>Email</label>
+        <input
+          type="email"
+          required
+          onChange={changeHandler}
+          name="email"
+          value={email}
+        />
 
-                <label>Confirm password</label>
-                <input type="text" required />
-                
-                <button type="submit">Sign Up</button>
-            </form>
-        </div>
-    )
-}
+        <label>Password</label>
+        <input
+          type="password"
+          required
+          onChange={changeHandler}
+          name="password"
+          value={password}
+        />
 
-export default SignUp
+        <label>Confirm password</label>
+        <input
+          type="password"
+          required
+          onChange={changeHandler}
+          name="confirmPassword"
+          value={confirmPassword}
+        />
+
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
