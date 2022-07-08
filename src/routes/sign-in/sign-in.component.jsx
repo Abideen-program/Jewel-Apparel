2724edfc62f3a6e95530df00fcd0c1111355ff72
Abideen Olafimihan <olafimihana@gmail.com>
2022-07-08 10:11:52 +0100

    leveraging the sign-up form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,4 +1,5 @@
 import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utilities/firebase/firebase.utilities"
+import SignUp from "../../components/sign-up/sign-up.component";
 
 const SignIn = () => {
     const logGoogleUser = async () =>  {
@@ -13,6 +14,7 @@ const SignIn = () => {
         <div>
             <h1>This is the sign up page</h1>
             <button onClick={logGoogleUser}>Sign up with Google</button>
+            <SignUp />
         </div>
     )
 }
