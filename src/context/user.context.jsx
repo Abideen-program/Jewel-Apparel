40be0d6db19e5dcd615285a943455b8b3cb0fbe4
Abideen Olafimihan <olafimihana@gmail.com>
2022-07-22 11:02:45 +0100

    formatting usercontext

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,17 +1,17 @@
-import { createContext, useState } from 'react'
+import { createContext, useState } from "react";
 
 //this is the value that needed to be access
 export const UserContext = createContext({
-    currentUser: null,
-    setCurrentUser: () => null,
-})
+  currentUser: null,
+  setCurrentUser: () => null,
+});
 
 /*this provider component will house the app.js so as all the component in app.js can 
 have access to the data*/
 export const UserProvider = ({ children }) => {
-    //setting the default value of current user to null
-    const [currentUser, setCurrentUser] = useState(null)
+  //setting the default value of current user to null
+  const [currentUser, setCurrentUser] = useState(null);
 
-    const value = {currentUser, setCurrentUser}
-    return <UserContext.Provider value={value}>{children}</UserContext.Provider>
-}
+  const value = { currentUser, setCurrentUser };
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
