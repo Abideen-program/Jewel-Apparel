eb09b1f7c2ab9cdc5d105031dd39321504a4784f
Abideen Olafimihan <olafimihana@gmail.com>
2022-08-07 14:59:11 +0100

    formating code

--- src/components/checkout/checkout.component.jsx
+++ src/components/checkout/checkout.component.jsx
@@ -1,30 +1,28 @@
-import { useContext } from 'react'
-import { CartContext } from '../../context/cart.context'
-import './checkout.styles.scss'
+import { useContext } from "react";
+import { CartContext } from "../../context/cart.context";
+import "./checkout.styles.scss";
 
 const CheckOut = () => {
-    const { cartItems, addItemToCart, removeItemFromCart} = useContext(CartContext)
-    return (
-        <div>
-            <h1>I am the checkout page</h1>
-            {
-                cartItems.map((cartItem) => {
-                    const {id, name, quantity} = cartItem
-                    return (
-                        <div key={id}>
-                            <h2>{name}</h2>
-                            <span>{quantity}</span>
-                            <br />
-                            <span onClick={() => removeItemFromCart(cartItem)}>decrement</span>
-                            <br />
-                            <span onClick={() => addItemToCart(cartItem)}>increament</span>
-                        </div>
-                    )
-                })
-            }
-            
-        </div>
-    )
-}
+  const { cartItems, addItemToCart, removeItemFromCart } =
+    useContext(CartContext);
+  return (
+    <div>
+      <h1>I am the checkout page</h1>
+      {cartItems.map((cartItem) => {
+        const { id, name, quantity } = cartItem;
+        return (
+          <div key={id}>
+            <h2>{name}</h2>
+            <span>{quantity}</span>
+            <br />
+            <span onClick={() => removeItemFromCart(cartItem)}>decrement</span>
+            <br />
+            <span onClick={() => addItemToCart(cartItem)}>increament</span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default CheckOut
+export default CheckOut;
