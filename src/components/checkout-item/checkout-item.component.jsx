ac8dc6e098a6c882887333bdde1e5431a6f843f0
Abideen Olafimihan <olafimihana@gmail.com>
2022-08-10 18:38:01 +0100

    formating code

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,30 +1,37 @@
-import { useContext } from 'react'
-import { CartContext } from '../../context/cart.context'
-import './checkout-item.styles.scss'
+import { useContext } from "react";
+import { CartContext } from "../../context/cart.context";
+import "./checkout-item.styles.scss";
 
-const CheckoutItem = ( {cartItem} ) => {
-    const { removeItemFromCheckout, addItemToCart, removeItemFromCart } = useContext(CartContext)
-    const { name, imageUrl, quantity, price } = cartItem;
+const CheckoutItem = ({ cartItem }) => {
+  const { removeItemFromCheckout, addItemToCart, removeItemFromCart } =
+    useContext(CartContext);
+  const { name, imageUrl, quantity, price } = cartItem;
 
-    const increaseQuantity = () => addItemToCart(cartItem)
-    const reduceQuantity = () => removeItemFromCart(cartItem)
-    const clearCheckout = () => removeItemFromCheckout(cartItem)
-    return (
-        <div className='checkout-item-container'>
-            <div className='image-container'>
-                <img src={imageUrl} alt={`${name}`} />
-            </div>
+  const increaseQuantity = () => addItemToCart(cartItem);
+  const reduceQuantity = () => removeItemFromCart(cartItem);
+  const clearCheckout = () => removeItemFromCheckout(cartItem);
+  return (
+    <div className="checkout-item-container">
+      <div className="image-container">
+        <img src={imageUrl} alt={`${name}`} />
+      </div>
 
-            <span className='name'>{name}</span>
-            <span className='quantity'>
-                <div className='arrow' onClick={reduceQuantity}>&#10094;</div>
-                <span className='value'>{quantity}</span>
-                <div className='arrow' onClick={increaseQuantity}>&#10095;</div>
-            </span>
-            <span className='price'>{price}</span>
-            <div className='remove-button' onClick={clearCheckout}>&#10005;</div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={reduceQuantity}>
+          &#10094;
         </div>
-    )
-}
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={increaseQuantity}>
+          &#10095;
+        </div>
+      </span>
+      <span className="price">{price}</span>
+      <div className="remove-button" onClick={clearCheckout}>
+        &#10005;
+      </div>
+    </div>
+  );
+};
 
-export default CheckoutItem
+export default CheckoutItem;
