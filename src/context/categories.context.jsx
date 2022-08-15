2ea9bb8d8bc297ca39325cea998cd4a49b887f4d
Abideen Olafimihan <olafimihana@gmail.com>
2022-08-15 23:40:34 +0100

    changing product context to categories context and pull data from firestore

---
+++ src/context/categories.context.jsx
@@ -0,0 +1,22 @@
+import { createContext, useState, useEffect } from 'react'
+
+import { getCollectionsAndDocuments } from '../utilities/firebase/firebase.utilities.js'
+
+export const CategoriesContext = createContext({
+    CategoriesMap: {}
+})
+
+export const CategoriesProvider = ({ children }) => {
+    const [CategoriesMap, setCategoriesMap] =  useState({})
+
+    useEffect(() => {
+        const getCategoriesMap = async  () => {
+            const categoryMap = await getCollectionsAndDocuments()
+            console.log(categoryMap)
+            setCategoriesMap(categoryMap)
+        }
+        getCategoriesMap()
+    }, [])
+    const value = {CategoriesMap, setCategoriesMap}
+    return <CategoriesContext.Provider value={value}> {children} </CategoriesContext.Provider>
+}
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,19 +1,25 @@
 // import SHOP_DATA from "../../shop-data.json";
-import { useContext } from "react";
+import { useContext, Fragment } from "react";
 import ProductCard from "../../components/product-card/product-card.component";
-import { ProductsContext } from "../../context/products.context";
-import './shop.styles.scss'
+import { CategoriesContext } from "../../context/categories.context";
+import "./shop.styles.scss";
 
 const Shop = () => {
-    const { products } = useContext(ProductsContext)
+  const { CategoriesMap } = useContext(CategoriesContext);
   return (
-    <div className="products-container">
-      {products.map((product) => {
-        return (
-          <ProductCard key={product.id} product={product}/>
-        );
+    <Fragment>
+      {Object.keys(CategoriesMap).map((title) => {
+        <Fragment key={title}>
+          <h2>{title}</h2>
+
+          <div className="products-container">
+            {CategoriesMap[title].map((product) => {
+              return <ProductCard key={product.id} product={product} />;
+            })}
+          </div>
+        </Fragment>;
       })}
-    </div>
+    </Fragment>
   );
 };
 
