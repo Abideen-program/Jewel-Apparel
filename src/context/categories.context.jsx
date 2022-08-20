ad1966b21cb5d3279d8cc639fd20cc7f8f63153e
Abideen Olafimihan <olafimihana@gmail.com>
2022-08-20 10:35:53 +0100

    formatting code

--- src/context/categories.context.jsx
+++ src/context/categories.context.jsx
@@ -1,22 +1,27 @@
-import { createContext, useState, useEffect } from 'react'
+import { createContext, useState, useEffect } from "react";
 
-import { getCollectionsAndDocuments } from '../utilities/firebase/firebase.utilities.js'
+import { getCollectionsAndDocuments } from "../utilities/firebase/firebase.utilities.js";
 
 export const CategoriesContext = createContext({
-    CategoriesMap: {}
-})
+  CategoriesMap: {},
+});
 
 export const CategoriesProvider = ({ children }) => {
-    const [CategoriesMap, setCategoriesMap] =  useState({})
+  const [CategoriesMap, setCategoriesMap] = useState({});
 
-    useEffect(() => {
-        const getCategoriesMap = async  () => {
-            const categoryMap = await getCollectionsAndDocuments()
-            console.log(categoryMap)
-            setCategoriesMap(categoryMap)
-        }
-        getCategoriesMap()
-    }, [])
-    const value = {CategoriesMap, setCategoriesMap}
-    return <CategoriesContext.Provider value={value}> {children} </CategoriesContext.Provider>
-}
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCollectionsAndDocuments();
+      console.log(categoryMap);
+      setCategoriesMap(categoryMap);
+    };
+    getCategoriesMap();
+  }, []);
+  const value = { CategoriesMap, setCategoriesMap };
+  return (
+    <CategoriesContext.Provider value={value}>
+      {" "}
+      {children}{" "}
+    </CategoriesContext.Provider>
+  );
+};
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,35 +1,33 @@
-import { useContext, useState, useEffect, Fragment } from 'react'
+import { useContext, useState, useEffect, Fragment } from "react";
 
-import { useParams } from 'react-router-dom'
+import { useParams } from "react-router-dom";
 
-import { CategoriesContext } from '../../context/categories.context'
+import { CategoriesContext } from "../../context/categories.context";
 
-import ProductCard from '../../components/product-card/product-card.component'
+import ProductCard from "../../components/product-card/product-card.component";
 
-import './category.styles.scss'
+import "./category.styles.scss";
 
 const Category = () => {
-const { category } = useParams()
-const { CategoriesMap } = useContext(CategoriesContext)
-const [products, setProducts] = useState(CategoriesMap[category])
+  const { category } = useParams();
+  const { CategoriesMap } = useContext(CategoriesContext);
+  const [products, setProducts] = useState(CategoriesMap[category]);
 
-useEffect(() => {
-    setProducts(CategoriesMap[category])    
-}, [category, CategoriesMap])
+  useEffect(() => {
+    setProducts(CategoriesMap[category]);
+  }, [category, CategoriesMap]);
 
-return(
+  return (
     <Fragment>
-        <h2 className='category-title'>{category.toUpperCase()}</h2>
-        <div className='category-container'>
-            {
-                products &&  products.map((product) => {
-                return <ProductCard key={product.id} product={product}/>
-            })
-            }
-        </div>
+      <h2 className="category-title">{category.toUpperCase()}</h2>
+      <div className="category-container">
+        {products &&
+          products.map((product) => {
+            return <ProductCard key={product.id} product={product} />;
+          })}
+      </div>
     </Fragment>
-)
+  );
+};
 
-}
-
-export default Category
+export default Category;
